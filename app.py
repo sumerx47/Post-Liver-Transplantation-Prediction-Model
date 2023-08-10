22ab2acbd2e159e0ca7ab61d299e6651417da367
mohammad sumer ali <sumerx47@gmail.com>
2023-08-10 16:31:57 +0530

    Deployed Successfully

--- app.py
+++ app.py
@@ -1,46 +1,88 @@
-from flask import Flask,request,render_template
-import numpy as np
-import pandas as pd
+import pickle
+from flask import Flask, render_template, request
 
-from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+app = Flask(__name__)
 
-application=Flask(__name__)
+pickle_in = open("model.pkl", "rb")
+best_model = pickle.load(pickle_in)
 
-app=application
+def liver_prediction(donor_age, donor_gender, donor_bmi, donor_cause_of_death, donor_diabetes, donor_hypertension, donor_alcohol_abuse, donor_smoking, donor_lymphocyte, donor_hepatitis_b, donor_hepatitis_c, recipient_etiology, recipient_meld_score, recipient_age, recipient_gender, recipient_bmi, recipient_diabetes, recipient_hypertension, recipient_alcohol_abuse, recipient_smoking, recipient_lymphocyte, recipient_hepatitis_b, recipient_hepatitis_c, recipient_albumin_level, recipient_alcoholic_cirrhosis, recipient_primary_biliary_cirrhosis, recipient_na, recipient_mg, recipient_wbc, recipient_platelets, recipient_cold_ischemia_time, recipient_warm_ischemia_time, recipient_blood_transfusion, recipient_immunosuppressant_medication, recipient_rejection_episodes):
 
-## Route for a home page
+    prediction = best_model.predict([[donor_age, donor_gender, donor_bmi, donor_cause_of_death, donor_diabetes, donor_hypertension, donor_alcohol_abuse, donor_smoking, donor_lymphocyte, donor_hepatitis_b, donor_hepatitis_c, recipient_etiology, recipient_meld_score, recipient_age, recipient_gender, recipient_bmi, recipient_diabetes, recipient_hypertension, recipient_alcohol_abuse, recipient_smoking, recipient_lymphocyte, recipient_hepatitis_b, recipient_hepatitis_c, recipient_albumin_level, recipient_alcoholic_cirrhosis, recipient_primary_biliary_cirrhosis, recipient_na, recipient_mg, recipient_wbc, recipient_platelets, recipient_cold_ischemia_time, recipient_warm_ischemia_time, recipient_blood_transfusion, recipient_immunosuppressant_medication, recipient_rejection_episodes]])
+    return prediction
 
-@app.route('/')
-def index():
-    return render_template('index.html') 
+@app.route("/", methods=["GET", "POST"])
+def main():
+    complication = ""
+    if request.method == "POST":
+        donor_age = int(request.form["donor_age"])
+        donor_gender_male = int(request.form["donor_gender_male"])
+        donor_gender_female = int(request.form["donor_gender_female"])
+        donor_bmi = float(request.form["donor_bmi"])
+        donor_cause_of_death = int(request.form["donor_cause_of_death"])
+        donor_diabetes = int(request.form["donor_diabetes"])
+        donor_hypertension = int(request.form["donor_hypertension"])
+        donor_alcohol_abuse = int(request.form["donor_alcohol_abuse"])
+        donor_smoking = int(request.form["donor_smoking"])
+        donor_lymphocyte = int(request.form["donor_lymphocyte"])
+        donor_hepatitis_b = int(request.form["donor_hepatitis_b"])
+        donor_hepatitis_c = int(request.form["donor_hepatitis_c"])
+        recipient_etiology = int(request.form["recipient_etiology"])
+        recipient_meld_score = float(request.form["recipient_meld_score"])
+        recipient_age = int(request.form["recipient_age"])
+        recipient_gender = int(request.form["recipient_gender"])
+        recipient_bmi = float(request.form["recipient_bmi"])
+        recipient_diabetes = int(request.form["recipient_diabetes"])
+        recipient_hypertension = int(request.form["recipient_hypertension"])
+        recipient_alcohol_abuse = int(request.form["recipient_alcohol_abuse"])
+        recipient_smoking = int(request.form["recipient_smoking"])
+        recipient_lymphocyte = float(request.form["recipient_lymphocyte"])
+        recipient_hepatitis_b = int(request.form["recipient_hepatitis_b"])
+        recipient_hepatitis_c = int(request.form["recipient_hepatitis_c"])
+        recipient_albumin_level = float(request.form["recipient_albumin_level"])
+        recipient_alcoholic_cirrhosis = float(request.form["recipient_alcoholic_cirrhosis"])
+        recipient_primary_biliary_cirrhosis = float(request.form["recipient_primary_biliary_cirrhosis"])
+        recipient_na = float(request.form["recipient_na"])
+        recipient_mg = float(request.form["recipient_mg"])
+        recipient_wbc = int(request.form["recipient_wbc"])
+        recipient_platelets = int(request.form["recipient_platelets"])
+        recipient_cold_ischemia_time = float(request.form["recipient_cold_ischemia_time"])
+        recipient_warm_ischemia_time = float(request.form["recipient_warm_ischemia_time"])
+        recipient_blood_transfusion = float(request.form["recipient_blood_transfusion"])
+        recipient_immunosuppressant_medication = int(request.form["recipient_immunosuppressant_medication"])
+        recipient_rejection_episodes = int(request.form["recipient_rejection_episodes"])
+
+        complication_index = liver_prediction(donor_age,donor_gender_male, donor_gender_female, donor_bmi, donor_cause_of_death, donor_diabetes, donor_hypertension, donor_alcohol_abuse, donor_smoking, donor_lymphocyte, donor_hepatitis_b, donor_hepatitis_c, recipient_etiology, recipient_meld_score, recipient_age, recipient_gender, recipient_bmi, recipient_diabetes, recipient_hypertension, recipient_alcohol_abuse, recipient_smoking, recipient_lymphocyte, recipient_hepatitis_b, recipient_hepatitis_c, recipient_albumin_level, recipient_alcoholic_cirrhosis, recipient_primary_biliary_cirrhosis, recipient_na, recipient_mg, recipient_wbc, recipient_platelets, recipient_cold_ischemia_time, recipient_warm_ischemia_time, recipient_blood_transfusion, recipient_immunosuppressant_medication, recipient_rejection_episodes)
+
+        if complication_index.size > 0:
+                # ...
+            if complication_index[0] == 0:
+                complication = '" Artery Thrombosis "'
+            elif complication_index[0] == 1:
+                complication = '" Biliary Complications "'
+            elif complication_index[0] == 2:
+                complication = '" Cardiovascular Complications "'
+            elif complication_index[0] == 3:
+                complication = '" Infection "'
+            elif complication_index[0] == 4:
+                complication = '" Metabolic Complications "'
+            elif complication_index[0] == 5:
+                complication = '" No Complication "'
+            elif complication_index[0] == 6:
+                complication = '" Portal Vein Thrombosis "'
+            elif complication_index[0] == 7:
+                complication = '" Post-transplant Diabetes "'
+            elif complication_index[0] == 8:
+                complication = '" Primary Graft Non-function "'
+            elif complication_index[0] == 9:
+                complication = '" Rejection "'
+            elif complication_index[0] == 10:
+                complication = '" Renal Dysfunction "'
+            else:
+                complication = 'Invalid prediction'
 
 
-@app.route('/predictdata', methods=['GET', 'POST']) 
-def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
-    else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+    return render_template("index.html", complication=complication)
 
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
-
-        predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
-    
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)  
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ request.py
@@ -0,0 +1,49 @@
+import requests
+
+# Specify the URL of your Flask app
+url = "http://localhost:5000"
+
+# Sample data for the input fields
+data = {
+    "donor_age": 35,
+    "donor_gender: 1,
+    "donor_bmi": 28.9,
+    "donor_cause_of_death": 0,
+    "donor_diabetes": 0,
+    "donor_hypertension": 1,
+    "donor_alcohol_abuse": 0,
+    "donor_smoking": 1,
+    "donor_lymphocyte": 0,
+    "donor_hepatitis_b": 0,
+    "donor_hepatitis_c": 0,
+    "recipient_etiology": 1,
+    "recipient_meld_score": 20,
+    "recipient_age": 50,
+    "recipient_gender": 0,
+    "recipient_bmi": 25.5,
+    "recipient_diabetes": 1,
+    "recipient_hypertension": 0,
+    "recipient_alcohol_abuse": 0,
+    "recipient_smoking": 0,
+    "recipient_lymphocyte": 0.8,
+    "recipient_hepatitis_b": 0,
+    "recipient_hepatitis_c": 1,
+    "recipient_albumin_level": 40,
+    "recipient_alcoholic_cirrhosis": 5.0,
+    "recipient_primary_biliary_cirrhosis": 0.9,
+    "recipient_na": 140,
+    "recipient_mg": 2.0,
+    "recipient_wbc": 7000,
+    "recipient_platelets": 250000,
+    "recipient_cold_ischemia_time": 8.0,
+    "recipient_warm_ischemia_time": 2.5,
+    "recipient_blood_transfusion": 1.0,
+    "recipient_immunosuppressant_medication": 2,
+    "recipient_rejection_episodes": 1
+}
+
+# Send a POST request to your Flask app
+response = requests.post(url, data=data)
+
+# Print the response from the Flask app
+print(response.text)
